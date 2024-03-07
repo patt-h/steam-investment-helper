1b04fe02dfb9cd99ca0121a08a72aefeefe75a90
patt-h <patryk.huchla@wp.pl>
2024-03-07 01:16:37 +0100

    Added React to build frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="topbar">
+      <div className="buttons">
+        <button>Main Page</button>
+        <button>Market</button>
+        <button>About</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
