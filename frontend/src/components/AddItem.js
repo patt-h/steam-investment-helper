74a0577ca85999204f498018d199c7240602f970
patt-h <patryk.huchla@wp.pl>
2024-03-11 14:22:53 +0100

    Added modal that opens after add button is clicked

--- frontend/src/components/AddItem.js
+++ frontend/src/components/AddItem.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './additems.css';
 
+const AddItem = ({open, onClose}) => {
+    if (!open) {
+        return null;
+    }
+    return (
+        <div className="overlay">
+            <div className="modal-container">
+                <p onClick={onClose} className="close-modal">X</p>
+                <div className="modal-content">
+                    <h1>Add items to track</h1>
+                    <table id="add-table" className="add-table-container">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Bought price</th>
+                                <th>Quantity</th>
+                                <th>Link to steam market</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><input type="text"></input></td>
+                                <td><input type="text"></input></td>
+                                <td><input type="text"></input></td>
+                                <td><input type="text"></input></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="button-container">
+                    <button className="submit-button">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+    )
+}
 
 export default AddItem;
--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import fetchData from './fetchData';
-//import AddItem from './AddItem';
+import AddItem from './AddItem';
 import './table.css';
 
 function Item() {
   const [data, setData] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     const fetchDataAndProcess = async () => {
@@ -66,11 +67,12 @@ function Item() {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
     <div>
+    <AddItem open={openModal} onClose={() => setOpenModal(false)} />
       <table className="table-container">
         <thead>
           <tr>
             <th>
-              <button className="add-button" title="Add new item">
+              <button className="add-button" title="Add new item" onClick={() => setOpenModal(true)}>
                 <span className="material-symbols-outlined">
                   <b>add</b>
                 </span>
@@ -96,7 +98,7 @@ function Item() {
               <td style={{backgroundColor: isNaN(parseFloat(sessionStorage.getItem(item.marketHashName))) ? "gray" : item.boughtPrice > parseFloat(sessionStorage.getItem(item.marketHashName)) ? "#e93030" : "#4abf26"}}>
                 <b>
                   <span className="profit">
-                  {(parseFloat(sessionStorage.getItem(item.marketHashName)) * parseInt(item.quantity)) - (parseFloat(item.boughtPrice) * parseInt(item.quantity))}zł
+                  {(parseFloat(sessionStorage.getItem(item.marketHashName)) * parseInt(item.quantity)).toFixed(2) - (parseFloat(item.boughtPrice) * parseInt(item.quantity)).toFixed(2)}zł
                   </span>
                 </b>
               </td>
